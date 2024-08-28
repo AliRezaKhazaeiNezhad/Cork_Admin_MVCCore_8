a7ae6d74692eb233f125c0df3a240c3fdd6383c3
amir hossein <amirhossein00978@gmail.com>
2024-08-28 07:04:19 +0330

    فونت های فارسی controller-account

---
+++ TGNH/TGNH.Panel/Models/AccountVeiwModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TGNH.Panel.Models
+{
+    public class AccountVeiwModel
+    {
+        public string Name { get; set; }
+        public int password { get; set; }
+
+        
+
+    }
+}
